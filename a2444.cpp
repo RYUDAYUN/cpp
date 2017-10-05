a9c28c43909e8698f259ce8a9006f32c05385b63
류다윤 <ekdbs3675@naver.com>
2017-10-05 18:15:10 +0900

    백준 2444번

---
+++ a2444.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+int main(){
+	int n,x,y;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		x=n-i;
+		y=2*i-1;
+		while(x){
+			printf(" ");
+			x--;
+		}
+		while(y){
+			printf("*");
+			y--;
+		}
+		printf("\n");
+	}
+	for(int i=n-1;i>0;i--){
+		x=n-i;
+		y=2*i-1;
+		while(x){
+			printf(" ");
+			x--;
+		}
+		while(y){
+			printf("*");
+			y--;
+		}
+		printf("\n");
+	}
+	return 0;
+}
