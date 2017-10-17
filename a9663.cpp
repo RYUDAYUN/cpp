5ee79191f4c03a3f7bb0bd8dbf1020a1f4a63415
류다윤 <ekdbs3675@naver.com>
2017-10-17 13:21:58 +0900

    백준 9663번 복잡도를 줄인 다른 풀이

--- a9663.cpp
+++ a9663.cpp
@@ -1,46 +1,37 @@
 #include <iostream>
 using namespace std;
-bool a[15][15];
 int n;
-int ans=0;
+bool check_col[15];
+bool check_dig[35];
+bool check_dig2[35];
 bool check(int row,int col){
-	for(int i=0;i<n;i++){
-		if(i==row)continue;
-		if(a[i][col])return false;
-	}
-	int x = row-1;
-	int y = col-1;
-	while(x>=0&&y>=0){
-		if(a[x][y])
-			return false;
-		x--;
-		y--;
-	}
-	x = row-1;
-	y = col+1;
-	while(x>=0&&y<n){
-		if(a[x][y])
-			return false;
-		x--;
-		y++;
-	}
+	if(check_col[col])
+		return false;
+	if(check_dig[row+col])
+		return false;
+	if(check_dig2[row-col+n])
+		return false;
 	return true;
 }
-void calc(int row){
-	if(row==n){
-		ans+=1;
-	}
+int calc(int row){
+	if(row==n)
+		return 1;
+	int cnt=0;
 	for(int col=0;col<n;col++){
-		a[row][col]=true;
 		if(check(row,col)){
-			calc(row+1);
+			check_dig[row+col]=true;
+			check_dig2[row-col+n]=true;
+			check_col[col]=true;
+			cnt+=calc(row+1);
+			check_dig[row+col]=false;
+			check_dig2[row-col+n]=false;
+			check_col[col]=false;
 		}
-		a[row][col]=false;
 	}
+	return cnt;
 }
 int main(){
 	cin>>n;
-	calc(0);
-	cout<<ans<<endl;
+	cout<<calc(0)<<'\n';
 	return 0;
 }
