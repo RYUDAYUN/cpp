934943b0d735d85f23829f1c8d17b4c08b52f2c8
류다윤 <ekdbs3675@naver.com>
2017-10-19 20:58:12 +0900

    백준 10844 dp이용 풀기

--- a2193.cpp
+++ a2193.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 int main(){
 	int n;
-	long long int d[100][2];
+	long long int d[101][10];
 	cin>>n;
-	d[1][1]=1;
+	for(int i=1;i<=9;i++)
+		d[1][i]=1;
 	d[1][0]=0;
-	d[2][0]=1;
-	d[2][1]=0;
-	for(int i=3;i<=n;i++){
-		d[i][0]=d[i-1][0]+d[i-1][1];
-		d[i][1]=d[i-1][0];
+	for(int i=2;i<=n;i++){
+		for(int j=0;j<10;j++){
+			if(j==0){
+				d[i][j]=d[i-1][1];
+			}else if(j==9){
+				d[i][j]=d[i-1][8];
+			}else{
+				d[i][j]=d[i-1][j+1]+d[i-1][j-1];
+			}
+		}
 	}
-	cout<<d[n][0]+d[n][1]<<endl;
+	int ans=0;
+	for(int i=0;i<10;i++){
+		ans+=d[n][i];
+	}
+	cout<<ans<<endl;
 	return 0;
 }
