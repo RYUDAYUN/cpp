546d21e1a782ae4bb520b5513081d6234bf78cd3
류다윤 <ekdbs3675@naver.com>
2017-10-21 17:29:56 +0900

    백준 1912번 dp이용해 풀기

---
+++ a1912.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	vector<int> a(n);
+	vector<int> d(n);
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	d[0]=a[0];
+	for(int i=1;i<n;i++){
+		d[i]=a[i];
+		d[i]=max(d[i],d[i-1]+a[i]);
+	}
+	cout<<*max_element(d.begin(),d.end())<<endl;
+	return 0;
+}
